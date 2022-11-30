ad4e709b2b9ecf8122394ea43f8a13239f8837ad
thorn573 <thorn573@umn.edu>
2022-11-29 21:25:40 -0600

    Implemented rocks & replication file output.

--- monkey.py
+++ monkey.py
@@ -5,9 +5,10 @@ import random
     #   Key: String path name
     #   Value: List where element 0 is length & element 1 is cumulative probability 
 class Monkey:
-    def __init__(self, pathDict, speed = 1):
+    def __init__(self, pathDict, speed = 1, rockRate = 0):
         self.pathDict = pathDict
         self.speed = speed
+        self.rockRate = rockRate
     
     def travelRandomPath(self):
         dunif = random.uniform(0, 1)
@@ -24,4 +25,9 @@ class Monkey:
             travelTime = self.pathDict[paths[i]][0] * self.speed
         
         return pathTraveled, travelTime
+    
+    def destroyKeyboard(self):
+        dunif = random.uniform(0, 1)
+
+        return dunif < self.rockRate
 
--- simulation.py
+++ simulation.py
@@ -1,5 +1,4 @@
 import random
-import statistics
 
 import keyboard
 import monkey
@@ -10,16 +9,20 @@ class Simulation:
             interArrivalRate = 1, 
             serviceRate = 1, 
             numMonkeys = 100,
+            aveMonkeySpeed = 1,
+            stdMonkeySpeed = 0,
             numMonkeysPerArrival = 1,
             numKeysPressedPerMonkey = 1,
             keyPathNames = ["Main"],
             pathDict = { "Main": [1, 1] }, 
             numRocks = 0, 
-            repairRate = 0,
+            repairRate = 1,
         ):
         self.interArrivalRate = interArrivalRate
         self.serviceRate = serviceRate
         self.numMonkeys = numMonkeys
+        self.aveMonkeySpeed = aveMonkeySpeed
+        self.stdMonkeySpeed = stdMonkeySpeed
         self.numMonkeysPerArrival = numMonkeysPerArrival
         self.numKeysPressedPerMonkey = numKeysPressedPerMonkey
         self.keyPathNames = keyPathNames
@@ -27,6 +30,8 @@ class Simulation:
         self.numDistractions = len(pathDict.keys()) - len(keyPathNames)
         self.numRocks = numRocks
         self.repairRate = repairRate
+
+        self.rockRate = self.numRocks / self.numMonkeys
 
         self.time = 0
 
@@ -39,33 +44,35 @@ class Simulation:
 
         self.correctWordList = []
 
-
     def run(self, numWords = 1):
-        # need rock
-        # print to csv, do stats in R
         simKeyboard = keyboard.Keyboard()
 
         while len(self.correctWordList) < numWords:
             recordedTime = 0
             interarrivalTime = random.expovariate(self.interArrivalRate) 
             for i in range(self.numMonkeysPerArrival):
-                simMonkey = monkey.Monkey(self.pathDict)
+                monkeySpeed = random.normalvariate(self.aveMonkeySpeed, self.stdMonkeySpeed)
+                simMonkey = monkey.Monkey(self.pathDict, speed = monkeySpeed, rockRate = self.rockRate)
                 pathTraveled, timeTaken = simMonkey.travelRandomPath()
 
                 serviceTime = 0
                 if pathTraveled in self.keyPathNames:
-                    serviceTime = random.expovariate(self.serviceRate)
-                    
-                    for j in range(self.numKeysPressedPerMonkey):
-                        keyPressed = simKeyboard.getKeyPress()
-                        if len(self.createdWordList[self.wordIndex]) < self.wordLength:
-                            self.createdWordList[self.wordIndex] += keyPressed
-                        else: 
-                            if self.createdWordList[self.wordIndex].lower() in self.knownWordList:
-                                self.correctWordList.append(self.createdWordList[self.wordIndex])
+                    if simMonkey.destroyKeyboard():
+                        repairTime = random.expovariate(self.repairRate)
+                        self.time += repairTime
+                    else:
+                        serviceTime = random.expovariate(self.serviceRate)
+                        
+                        for j in range(self.numKeysPressedPerMonkey):
+                            keyPressed = simKeyboard.getKeyPress()
+                            if len(self.createdWordList[self.wordIndex]) < self.wordLength:
+                                self.createdWordList[self.wordIndex] += keyPressed
+                            else: 
+                                if self.createdWordList[self.wordIndex].lower() in self.knownWordList:
+                                    self.correctWordList.append(self.createdWordList[self.wordIndex])
 
-                            self.createdWordList.append(keyPressed)
-                            self.wordIndex += 1 
+                                self.createdWordList.append(keyPressed)
+                                self.wordIndex += 1 
 
                 if timeTaken + interarrivalTime + serviceTime > recordedTime:
                     recordedTime = timeTaken + interarrivalTime + serviceTime
@@ -73,16 +80,31 @@ class Simulation:
             self.time += recordedTime
 
         return self.time, self.correctWordList
+    
+    def replicate(self, n = 10, writeFile = False):
+        # add ave service time, ave repair time, num of keyboards destroyed, num monkeys, num keys pressed, etc. to CSV
+        fileLinesList = []
+        wordList = []
+
+        timeSum = 0
+        for i in range(n):
+            simTime, simWordList = Simulation().run()
+            timeSum += simTime
+            wordList += simWordList
+
+            simWordString = " ".join(simWordList)
+            fileLinesList.append(f"{i + 1}, {simWordString}, {simTime}\n")
+
+        timeMean = timeSum / n
+        print(f"Mean time: {timeMean}")
+        print(f"Words generated: {wordList}")
+
+        if writeFile:
+            file = open("monkey_data.csv", "w")
+            file.write(f"AVE_TIME, {timeMean}\n")
+            file.write("INDEX, WORD, TIME\n")
+            file.writelines(fileLinesList)
+            file.close()
 
 if __name__ == "__main__":
-    timeList = []
-    wordList = []
-
-    numReplications = 10
-    for i in range(numReplications):
-        simTime, simWordList = Simulation().run()
-        timeList.append(simTime)
-        wordList += simWordList
-    
-    timeMean = statistics.mean(timeList)
-    print(timeMean)
+    Simulation().replicate(writeFile = True)
