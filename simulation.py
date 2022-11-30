2a1e382da86db9e98566fefcb548fe5fafe346ea
thorn573 <thorn573@umn.edu>
2022-11-30 09:41:21 -0600

    Expanded output data fields.

--- simulation.py
+++ simulation.py
@@ -13,6 +13,7 @@ class Simulation:
             stdMonkeySpeed = 0,
             numMonkeysPerArrival = 1,
             numKeysPressedPerMonkey = 1,
+            numWords = 1,
             keyPathNames = ["Main"],
             pathDict = { "Main": [1, 1] }, 
             numRocks = 0, 
@@ -25,6 +26,7 @@ class Simulation:
         self.stdMonkeySpeed = stdMonkeySpeed
         self.numMonkeysPerArrival = numMonkeysPerArrival
         self.numKeysPressedPerMonkey = numKeysPressedPerMonkey
+        self.numWords = numWords
         self.keyPathNames = keyPathNames
         self.pathDict = pathDict
         self.numDistractions = len(pathDict.keys()) - len(keyPathNames)
@@ -44,15 +46,44 @@ class Simulation:
 
         self.correctWordList = []
 
-    def run(self, numWords = 1):
+    def run(self):
+        numDistractions = 0
+        numKeysPressed = 0
+        
+        monkeySpeedSum = 0
+        numMonkeys = 0
+        minMonkeySpeed = float("inf")
+        maxMonkeySpeed = -float("inf")
+        aveMonkeySpeed = 0
+
+        serviceTimeSum = 0
+        numService = 0
+        minServiceTime = float("inf")
+        maxServiceTime = -float("inf")
+        aveServiceTime = 0
+
+        repairTimeSum = 0
+        numRepairs = 0
+        minRepairTime = float("inf")
+        maxRepairTime = -float("inf")
+        aveRepairTime = 0
+
         simKeyboard = keyboard.Keyboard()
 
-        while len(self.correctWordList) < numWords:
+        while len(self.correctWordList) < self.numWords:
             recordedTime = 0
             interarrivalTime = random.expovariate(self.interArrivalRate) 
             for i in range(self.numMonkeysPerArrival):
-                monkeySpeed = random.normalvariate(self.aveMonkeySpeed, self.stdMonkeySpeed)
+                monkeySpeed = abs(random.normalvariate(self.aveMonkeySpeed, self.stdMonkeySpeed))
                 simMonkey = monkey.Monkey(self.pathDict, speed = monkeySpeed, rockRate = self.rockRate)
+                
+                numMonkeys += 1
+                monkeySpeedSum += monkeySpeed
+                if monkeySpeed > maxMonkeySpeed:
+                    maxMonkeySpeed = monkeySpeed
+                if monkeySpeed < minMonkeySpeed:
+                    minMonkeySpeed = monkeySpeed
+
                 pathTraveled, timeTaken = simMonkey.travelRandomPath()
 
                 serviceTime = 0
@@ -60,11 +91,28 @@ class Simulation:
                     if simMonkey.destroyKeyboard():
                         repairTime = random.expovariate(self.repairRate)
                         self.time += repairTime
+
+                        numRepairs += 1
+                        repairTimeSum += repairTimeSum
+                        if repairTime > maxRepairTime:
+                            maxRepairTime = repairTime
+                        if repairTime < minRepairTime: 
+                            minRepairTime = repairTime
+                         
                     else:
                         serviceTime = random.expovariate(self.serviceRate)
+
+                        numService += 1
+                        serviceTimeSum += serviceTime
+                        if serviceTime > maxServiceTime:
+                            maxServiceTime = serviceTime
+                        if serviceTime < minServiceTime:
+                            minServiceTime = serviceTime
                         
                         for j in range(self.numKeysPressedPerMonkey):
                             keyPressed = simKeyboard.getKeyPress()
+                            numKeysPressed += 1
+
                             if len(self.createdWordList[self.wordIndex]) < self.wordLength:
                                 self.createdWordList[self.wordIndex] += keyPressed
                             else: 
@@ -73,36 +121,96 @@ class Simulation:
 
                                 self.createdWordList.append(keyPressed)
                                 self.wordIndex += 1 
+                else: 
+                    numDistractions += 1
 
                 if timeTaken + interarrivalTime + serviceTime > recordedTime:
                     recordedTime = timeTaken + interarrivalTime + serviceTime
           
             self.time += recordedTime
 
-        return self.time, self.correctWordList
+        if numMonkeys > 0:
+            aveMonkeySpeed = monkeySpeedSum / numMonkeys
+        if numService > 0:
+            aveServiceTime = serviceTimeSum / numService
+        if numRepairs > 0:
+            aveRepairTime = repairTimeSum / numRepairs
+        
+        outDict = {
+            "Time": self.time, 
+            "AllWordList": self.createdWordList,
+            "CorrectWordList": self.correctWordList,
+            "NumDistractions": numDistractions,
+            "NumKeysPressed": numKeysPressed,
+            
+            "NumMonkeys": numMonkeys,
+            "MinMonkeySpeed": minMonkeySpeed,
+            "MaxMonkeySpeed": maxMonkeySpeed,
+            "AveMonkeySpeed": aveMonkeySpeed, 
+            
+            "NumService": numService,
+            "MinServiceTime": minServiceTime,
+            "MaxServiceTime": maxServiceTime,
+            "AveServiceTime": aveServiceTime,
+
+            "NumRepairs": numRepairs,
+            "MinRepairTime": minRepairTime,
+            "MaxRepairTime": maxRepairTime,
+            "AveRepairTime": aveRepairTime,
+        }
+
+        return outDict
     
     def replicate(self, n = 10, writeFile = False):
-        # add ave service time, ave repair time, num of keyboards destroyed, num monkeys, num keys pressed, etc. to CSV
         fileLinesList = []
-        wordList = []
+        correctWordList = []
 
         timeSum = 0
         for i in range(n):
-            simTime, simWordList = Simulation().run()
-            timeSum += simTime
-            wordList += simWordList
+            simDict = Simulation().run()
 
-            simWordString = " ".join(simWordList)
-            fileLinesList.append(f"{i + 1}, {simWordString}, {simTime}\n")
+            simTime = simDict["Time"]
+            simCorrectWords = simDict["CorrectWordList"]
+            simAllWords = simDict["AllWordList"]
+            simNumDistractions = simDict["NumDistractions"]
+            simNumKeysPressed = simDict["NumKeysPressed"]
+
+            numMonkeys = simDict["NumMonkeys"]
+            minMonkeySpeed = simDict["MinMonkeySpeed"]
+            maxMonkeySpeed = simDict["MaxMonkeySpeed"]
+            aveMonkeySpeed = simDict["AveMonkeySpeed"]
+
+            numService = simDict["NumService"]
+            minServiceTime = simDict["MinServiceTime"]
+            maxServiceTime = simDict["MaxServiceTime"]
+            aveServiceTime = simDict["AveServiceTime"]
+
+            numRepairs = simDict["NumRepairs"]
+            minRepairTime = simDict["MinRepairTime"]
+            maxRepairTime = simDict["MaxRepairTime"]
+            aveRepairTime = simDict["AveRepairTime"]
+
+            timeSum += simTime
+            correctWordList += simCorrectWords
+
+            correctWordString = " ".join(simCorrectWords)
+            numAllWords = len(simAllWords)
+
+            fileLinesList.append(f"{i + 1}, {simTime}, {correctWordString}, {numAllWords}, {simNumDistractions}, {simNumKeysPressed}, " +
+                                 f"{numMonkeys}, {minMonkeySpeed}, {maxMonkeySpeed}, " + 
+                                 f"{numService}, {minServiceTime}, {maxServiceTime}, {aveServiceTime}, " + 
+                                 f"{numRepairs}, {minRepairTime}, {maxRepairTime}, {aveRepairTime} \n")
 
         timeMean = timeSum / n
         print(f"Mean time: {timeMean}")
-        print(f"Words generated: {wordList}")
+        print(f"Words generated: {correctWordList}")
 
         if writeFile:
-            file = open("monkey_data.csv", "w")
-            file.write(f"AVE_TIME, {timeMean}\n")
-            file.write("INDEX, WORD, TIME\n")
+            file = open("MonkeyData.csv", "w")
+            file.write("INDEX, TIME, CORRECT_WORDS, NUM_ALL_WORDS, NUM_DISTRACTIONS, NUM_KEYS_PRESSED, " +
+                       "NUM_MONKEYS, MIN_MONKEY_SPEED, MAX_MONKEY_SPEED, " + 
+                       "NUM_SERVICE, MIN_SERVICE_TIME, MAX_SERVICE_TIME, AVE_SERVICE_TIME, " + 
+                       "NUM_REPAIRS, MIN_REPAIR_TIME, MAX_REPAIR_TIME, AVE_REPAIR_TIME \n")
             file.writelines(fileLinesList)
             file.close()
 
